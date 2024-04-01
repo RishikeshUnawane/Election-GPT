345b253540a1e4e17bdd400c7f2d95171c6050e6
Rishikesh Unawane <rishikesh.unawane20@vit.edu>
2024-04-01 13:51:29 +0530

    Chat page logic updated

--- app.py
+++ app.py
@@ -72,96 +72,56 @@ def login():
         if user:
             get_userdId = user.get('user_Id')
             if bcrypt.checkpw(request.form['password'].encode('utf-8'), user['encrypt_pass']):
-                # new_chat = str(uuid.uuid4())
-                # chats.insert_one({
-                #     'user_Id' : get_userdId,
-                #     'chat_Id' : new_chat,
-                #     'chat_title' : 'How many seats are there in Rajya Sabha?',
-                #     'timestamp' : datetime.utcnow(),
-                #     'responses' : [
-                #     {
-                #     'question' : 'How many seats are there in Rajya Sabha?',
-                #     'answer' : 'There are total 288 seats in Rajya Sabha'
-                #     }]
-                # })
-                # user_chats = chats.find({"user_Id": get_userdId}).sort("user_Id", 1)
-                # for chat in user_chats:
-                #     print(chat)
-                return redirect(url_for('chat', username=username, user_Id=get_userdId))
-                # flash('Login successful.', 'success')
-            # Add any additional logic, such as session management
+                new_chat = str(uuid.uuid4())
+                chats.insert_one({
+                    'user_Id' : get_userdId,
+                    'chat_Id' : new_chat,
+                    'chat_title' : '',
+                    'response': [],
+                    'timestamp' : datetime.utcnow()
+                })
+                return redirect(url_for('chat', username=username, user_Id=get_userdId, chat_Id=new_chat))
         else:
             flash('Invalid username or password. Please try again.', 'danger')
 
     return render_template('login.html')
 
-@app.route('/chat/<username>/<user_Id>', methods=['GET', 'POST'])
-def chat(username, user_Id):
-    # if 'username' not in session:
-    #     return redirect(url_for('login'))
-    
+@app.route('/chat/<username>/<user_Id>/<chat_Id>', methods=['GET', 'POST'])
+def chat(username, user_Id, chat_Id):
+    # chat_history = None
     if request.method == 'POST':
         # user_id = session['user_id']  # Assuming you have user_id stored in session
         message_text = request.form['message']
-        # Save user message to the database
-        # message = Message(user_id, message_text)
-        # mongo.db.messages.insert_one(message.__dict__)
-
-        # Here you would send the user's question to Google Palm's API
-        # Get the response from the API and display it back to the user
-        # chain = get_qa_chain()
-        # response = chain(message_text)
-        new_chat = str(uuid.uuid4())
         response = get_qa_chain()(message_text)
-        # Extract relevant information from the response dictionary
-        # print(response['result'])
 
-        # Insert the chat into MongoDB
-        chat = {
-            'user_Id': user_Id,
-            'chat_Id': new_chat,
-            'message': message_text,
-            'response': {
-                'question': message_text,
-                'answer': response['result']
-            },
-            'timestamp': datetime.utcnow()
-        }
-        chats.insert_one(chat)
-        chat_history = chats.find({'user_Id': user_Id})
-        return render_template('chat.html', username=username, user_Id=user_Id, response=response['result'], chat_history=chat_history)
-
-    # Fetch chat history from the database and pass it to the template
-    # chat_history = chats.find({'user_Id': user_Id})
-    # print(chat_history)
-    return render_template('chat.html', username=username, user_Id=user_Id, chat_history=chat_history)
-
-@app.route('/populateDB', methods=["GET"])
-def populateDB():
-    users.insert_one({
-        'username' : 'Jhon',
-        'encrypt_pass' : 'hello@1$',
-        'voter_Id' : 'DL/01/001/000000',
-        'user_Id' : '1234567890123456',
-    })
-    user = users.insert_one({
-        'username' : 'Mia',
-        'encrypt_pass' : 'hello@1$',
-        'voter_Id' : 'DL/01/001/000000',
-        'user_Id' : '1234567820123456',
-    })
-    chats.insert_one({
-        'user_Id' : '1234567820123456',
-        'chat_Id' : '1234564890123432',
-        'chat_title' : 'How many seats are there in Rajya Sabha?',
-        'timestamp' : datetime.utcnow(),
-        'responses' : [
-        {
-        'question' : 'How many seats are there in Rajya Sabha?',
-        'answer' : 'There are total 288 seats in Rajya Sabha'
-        }]
-    })
-    return "DB updated with sample data!" 
+        chat_document = chats.find_one({'user_Id': user_Id, 'chat_Id': chat_Id})
+        if chat_document:
+            update_data = {
+                '$push': {
+                    'response': {
+                        'question': message_text,
+                        'answer' : response['result']
+                    }  
+                }
+            }
+            chats.update_one({'user_Id': user_Id, 'chat_Id': chat_Id}, update_data)
+        updated_document = chats.find_one({'user_Id': user_Id, 'chat_Id': chat_Id})
+        chat_history = updated_document['response']
+        result_list = []
+        for data in chat_history:
+            question = data['question']
+            answer = data['answer']
+            result_list.append((question, answer))
+        return render_template('chat.html', username=username, user_Id=user_Id, chat_Id=chat_Id, result_list=result_list)
+    updated_document = chats.find_one({'user_Id': user_Id, 'chat_Id': chat_Id})
+    chat_history = updated_document['response']
+    result_list = []
+    for data in chat_history:
+        question = data['question']
+        answer = data['answer']
+        result_list.append((question, answer))
+    print('result dictionary', result_list)
+    return render_template('chat.html', username=username, user_Id=user_Id, chat_Id=chat_Id, result_list=result_list)
 
 # main driver function
 if __name__ == '__main__':
