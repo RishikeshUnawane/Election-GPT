c31bd478748a0f7c3d9ddbb6257bc65376b76449
Rishikesh Unawane <rishikesh.unawane20@vit.edu>
2024-04-08 12:33:49 +0530

    backend logic complete

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_session import Session
+from functools import wraps
 from pymongo import MongoClient, ReturnDocument
 from pymongo.server_api import ServerApi
 from datetime import datetime
@@ -11,6 +13,9 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 app.secret_key = os.getenv("APP_SECRET")  # Replace with a strong secret key
 
 #Setting up Cloud DB
@@ -30,17 +35,23 @@ db = client.electiongpt
 users = db.users
 chats = db.chats
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'username' not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def register():
+    # New user will register and the username and password(hashed) will be saved in the database in the user collection
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         voter_id = request.form['voterId']
+        # while registration unique userID is created for the user
         new_user_id = str(uuid.uuid4())
 
         # Check if the username already exists
@@ -52,8 +63,8 @@ def register():
             users.insert_one({
                 'username': username,
                 'encrypt_pass': hashed,
-                'voter_id' : voter_id,  
-                'user_Id': new_user_id #needs to be changed 
+                'voter_id' : voter_id,  #add voter id validation here
+                'user_Id': new_user_id   
                 })
             
             flash('Registration successful. You can now log in.', 'success')
@@ -67,6 +78,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
+        session['username'] = username
         # Check if the username and password match
         user = users.find_one({'username': username})
         if user:
@@ -87,10 +99,10 @@ def login():
     return render_template('login.html')
 
 @app.route('/chat/<username>/<user_Id>/<chat_Id>', methods=['GET', 'POST'])
+@login_required
 def chat(username, user_Id, chat_Id):
-    # chat_history = None
+    
     if request.method == 'POST':
-        # user_id = session['user_id']  # Assuming you have user_id stored in session
         message_text = request.form['message']
         response = get_qa_chain()(message_text)
 
@@ -111,13 +123,10 @@ def chat(username, user_Id, chat_Id):
         
         all_chats = chats.find({'user_Id': user_Id})
         chat_list = list(all_chats)
-        print('all chat', all_chats)
         updated_document = chats.find_one({'user_Id': user_Id, 'chat_Id': chat_Id})
         chat_history = updated_document['response']
         if updated_document is not None:
             chat_history = updated_document.get('response', [])
-            # for chat in chat_history:
-            #     print('Chat', chat)
         else:
             print("Document not found")
         result_list = []
@@ -129,17 +138,36 @@ def chat(username, user_Id, chat_Id):
     
     all_chats = chats.find({'user_Id': user_Id})
     chat_list = list(all_chats)
-    print('all chat', all_chats)
     updated_document = chats.find_one({'user_Id': user_Id, 'chat_Id': chat_Id})
     chat_history = updated_document['response']
-    print('updated', updated_document)
     result_list = []
     for data in chat_history:
         question = data['question']
         answer = data['answer']
         result_list.append((question, answer))
-    print('result dictionary', result_list)
+    
     return render_template('chat.html', username=username, user_Id=user_Id, chat_Id=chat_Id, result_list=result_list, chat_list=chat_list)
+
+@app.route('/newchat/<username>/<user_Id>', methods=['GET'])
+@login_required
+def newchat(username, user_Id):
+    new_chat_Id = str(uuid.uuid4())
+    # Insert the new chat into the database
+    chats.insert_one({
+        'user_Id': user_Id,
+        'chat_Id': new_chat_Id,
+        'chat_title': 'New Chat',
+        'response': [],
+        'timestamp': datetime.utcnow()
+    })
+    # Redirect the user back to the chat page after creating the chat
+    return redirect(url_for('chat', username=username, user_Id=user_Id, chat_Id=new_chat_Id))
+
+@app.route('/logout', methods=['POST', 'GET'])
+@login_required
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 # main driver function
 if __name__ == '__main__':
